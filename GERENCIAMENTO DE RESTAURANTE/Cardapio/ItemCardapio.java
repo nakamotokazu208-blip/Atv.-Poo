bbfa4e5ff21aa9af7f836a1c07f195066779ab8e
Kazunorl27 <nakamotokazu208@gmail.com>
2025-11-23 17:30:59 -0300

    Create abstract class ItemCardapio for menu items

---
+++ GERENCIAMENTO DE RESTAURANTE/Cardapio/ItemCardapio.java
@@ -0,0 +1,19 @@
+public abstract class ItemCardapio {
+    private String nome; 
+    private double precoBase;
+
+    public ItemCardapio(String nome, double precoBase) {
+        this.nome = nome;
+        this.precoBase = precoBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public abstract double calcularPrecoFinal();
+}
